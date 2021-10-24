1b675226cfe529228b17ce3f9570e1567ff4c0ae
gautam <gautamk.ms@gmail.com>
2021-10-24 14:00:30 +0530

    new changes

--- src/test/java/com/example/demo/DockerJenkinsIntegrationApplicationTests.java
+++ src/test/java/com/example/demo/DockerJenkinsIntegrationApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@ExtendWith( SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class DockerJenkinsIntegrationApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void getsAllRides() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/demo/hi")
+			.accept(MediaType.APPLICATION_JSON))
+			.andReturn();
 	}
 
 }
